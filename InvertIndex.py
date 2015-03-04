51abab5f37c7ede23af70ddc255a5f0bcae410cc
SiddarthaNagireddy <siddartha.nagireddy@gmail.com>
2015-03-04 16:16:42 +0000

    added comments

--- InvertIndex.py
+++ InvertIndex.py
@@ -1,6 +1,5 @@
 '''
 Created on Feb 20, 2015
-
 @author: Siddartha.Reddy
 '''
 
@@ -9,6 +8,8 @@ from collections import defaultdict
 from nltk import PorterStemmer
 from array import array
 
+
+''' Using PorterStemmer '''
 p = PorterStemmer()
 
 class invertedindex:
@@ -17,9 +18,10 @@ class invertedindex:
         self.index=defaultdict(list) 
 
     def createindex(self):
+		''' Using a sample testfile which consists of huge text '''
         with open('C:/Users/Siddartha.Reddy/Desktop/testfiles/testfile.txt','r') as fd:
             for row in fd:
-                demotext = row.split('$')[1]
+                demotext = row.split('$')[1] # $ is the delimiter in my file
                 lineid = row.split('$')[0]
                 self.getStopwords()
                 terms = self.getTerms(demotext)
@@ -37,13 +39,15 @@ class invertedindex:
                     
                 ##print(self.index)
         self.searchword()
-
+	
+	''' optional stopwords removal '''
     def getStopwords(self):
         f = open('C:/Users/Siddartha.Reddy/Downloads/InvertedIndex/stopwords.dat','r')
         stopwords = [line.rstrip() for line in f]
         self.sw = dict.fromkeys(stopwords)
         f.close()
     
+	''' Tokenize and stem '''
     def getTerms(self,demotext):
         line = demotext.lower()
         line=re.sub(r'[^a-z0-9 ]',' ',line) # puts spaces instead of alpha numeric obj
@@ -51,7 +55,8 @@ class invertedindex:
         line = [x for x in line if x not in self.sw]
         # line = [p.stem(word) for word in line]
         return line
-
+	
+	''' builds the index '''
     def writeindex(self):
         f = open('indexfile','w')
         for term in self.index.iterkeys():
@@ -62,7 +67,8 @@ class invertedindex:
                 postinglist.append(':'.join([str(docID) ,','.join(map(str,positions))]))
             print >> f,''.join((term,'|',';'.join(postinglist)))
     
-    def searchword(self):
+	''' Search function - goes through the index and returns the document id's '''
+    def searchword(self): 
         searchword = 'antivirus'
         for term in self.index.iterkeys():
             if term == searchword:
